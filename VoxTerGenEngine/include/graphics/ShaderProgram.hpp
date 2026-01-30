5294280dc90184da7c2c72c82f68df3ce7d639a8
Mario <mariozak@tutanota.com>
2026-01-30 19:58:41 +0100

    added includes

--- VoxTerGenEngine/include/graphics/ShaderProgram.hpp
+++ VoxTerGenEngine/include/graphics/ShaderProgram.hpp
@@ -3,6 +3,12 @@
 
 #include <glad/glad.h>
 
+#include "glm/vec2.hpp"
+#include "glm/vec3.hpp"
+#include "glm/mat2x2.hpp"
+#include "glm/mat3x3.hpp"
+#include "glm/mat4x4.hpp"
+
 #include <string>
 #include <string_view>
 #include <unordered_map>
