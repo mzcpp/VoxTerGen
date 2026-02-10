3965a92d8d50978e154135f6c4e8a39bd1c40e09
Mario <mariozak@tutanota.com>
2026-02-10 19:44:12 +0100

    naive meshing WIP

--- VoxTerGenEngine/include/mesh/MeshBuilder.hpp
+++ VoxTerGenEngine/include/mesh/MeshBuilder.hpp
@@ -10,10 +10,14 @@
 class MeshBuilder
 {
 private:
-public:
-	static Mesh BuildMeshNaive(const Chunk& chunk, std::function<Block(int, int, int)> neighbor_query);
 
-	static Mesh BuildMeshGreedy(const Chunk& chunk, std::function<Block(int, int, int)> neighbor_query);
+public:
+	static Mesh BuildMeshNaive(const Chunk& chunk, std::function<Block(glm::ivec2, int, int, int, Direction)> neighbor_query);
+
+	static Mesh BuildMeshGreedy(const Chunk& chunk, std::function<Block(glm::ivec2, int, int, int, Direction)> neighbor_query);
+
+private:
+	void GetQuadMesh(const Chunk& chunk, int x, int y, int z, std::function<Block(glm::ivec2, int, int, int, Direction)> neighbor_query, Direction dir, Mesh& result_mesh) const;
 };
 
 #endif // MESH_BUILDER_HPP
