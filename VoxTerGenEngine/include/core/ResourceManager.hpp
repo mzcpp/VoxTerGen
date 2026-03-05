d99b2011222c197f3b55cb4879f654896f656f57
Mario <mariozak@tutanota.com>
2026-03-05 17:12:15 +0100

    first version of resource manager implemented

--- VoxTerGenEngine/include/core/ResourceManager.hpp
+++ VoxTerGenEngine/include/core/ResourceManager.hpp
@@ -1,12 +1,26 @@
 #ifndef RESOURCE_MANAGER_HPP
 #define RESOURCE_MANAGER_HPP
 
+#include "graphics/Texture2D.hpp"
+#include "graphics/ShaderProgram.hpp"
+
+#include <unordered_map>
+#include <string>
+
 class ResourceManager
 {
 private:
+    std::unordered_map<std::string, std::unique_ptr<Texture2D>> textures_;
+    std::unordered_map<std::string, std::unique_ptr<ShaderProgram>> shader_programs_;
+    // fonts
+    // sounds
 
 public:
+    ResourceManager();
 
+    Texture2D* GetTexture(const std::string& texture_name);
+
+    ShaderProgram* GetShader(const std::string& shader_name);
 };
 
 #endif // RESOURCE_MANAGER_HPP
--- VoxTerGenEngine/src/core/ResourceManager.cpp
+++ VoxTerGenEngine/src/core/ResourceManager.cpp
@@ -1,1 +1,33 @@
 #include "core/ResourceManager.hpp"
+#include "utils/Logger.hpp"
+
+ResourceManager::ResourceManager();
+{
+
+}
+
+Texture2D* ResourceManager::GetTexture(const std::string& texture_name)
+{
+    const auto texture_it = textures_.find(texture_name);
+
+    if (texture_it == textures_.end())
+    {
+        Logger::Log(LogLevel::ERROR, "Failed to get a texture! Texture name: {}", texture_name);
+        return nullptr;
+    }
+
+    return texture_it->second.get();
+}
+
+ShaderProgram* ResourceManager::GetShader(const std::string& shader_name)
+{
+    const auto shader_it = shader_programs_.find(shader_name);
+
+    if (shader_it == shader_programs_.end())
+    {
+        Logger::Log(LogLevel::ERROR, "Failed to get a shader program! Shader name: {}", shader_name);
+        return nullptr;
+    }
+
+    return shader_it->second.get();
+}
