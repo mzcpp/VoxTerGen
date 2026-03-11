5323a9382f30e7503f2aae5123e307c88f40b182
Mario <mariozak@tutanota.com>
2026-03-11 16:04:49 +0100

    basic opengl render objects added

--- VoxTerGenEngine/src/render/RenderMesh.cpp
+++ VoxTerGenEngine/src/render/RenderMesh.cpp
@@ -1,1 +1,17 @@
 #include "render/RenderMesh.hpp"
+
+#include <glad/glad/glad.h>
+
+RenderMesh::RenderMesh()
+{
+    glCreateVertexArray(1, &vao_);
+    glCreateBuffers(1, &vbo_):
+    glCreateBuffers(1, &ebo_):
+}
+
+RenderMesh::~RenderMesh()
+{
+    glDeleteVertexArrays(1, &vao_);
+    glDeleteBuffers(1, &vbo_);
+    glDeleteBuffers(1, &ebo_);
+}
