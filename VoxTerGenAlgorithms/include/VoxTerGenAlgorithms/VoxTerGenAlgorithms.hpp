6232d4340161ac1d1f8bc73fd715dc258cf7a63c
Mario <mariozak@tutanota.com>
2026-04-03 17:04:43 +0200

    static lib project configuration

---
+++ VoxTerGenAlgorithms/include/VoxTerGenAlgorithms/VoxTerGenAlgorithms.hpp
@@ -0,0 +1,15 @@
+#ifndef VOXTERGEN_ALGORITHMS
+#define VOXTERGEN_ALGORITHMS
+
+namespace voxtergen_algorithms
+{
+	class VoxTerGenAlgorithms
+	{
+	private:
+
+	public:
+		static void Test(int x, int y, int z);
+	};
+} // voxtergen_algorithms
+
+#endif
---
+++ VoxTerGenAlgorithms/src/VoxTerGenAlgorithms.cpp
@@ -0,0 +1,11 @@
+#include "pch.h"
+
+#include "VoxTerGenAlgorithms/VoxTerGenAlgorithms.hpp"
+
+#include <iostream>
+
+// TODO: This is an example of a library function
+void VoxTerGenAlgorithms::Test(int x, int y, int z)
+{
+	std::cout << "Hello from Algorithms project!\n";
+}
