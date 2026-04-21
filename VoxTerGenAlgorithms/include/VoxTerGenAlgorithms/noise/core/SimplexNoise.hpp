0f2f4c02a4abd2d2c4f6e40625356446581ff953
Mario <mariozak@tutanota.com>
2026-04-21 20:03:52 +0200

    moved implementation into proper class

--- VoxTerGenAlgorithms/include/VoxTerGenAlgorithms/noise/core/SimplexNoise.hpp
+++ VoxTerGenAlgorithms/include/VoxTerGenAlgorithms/noise/core/SimplexNoise.hpp
@@ -1,27 +1,21 @@
-#ifndef OPEN_SIMPLEX_2_NOISE_HPP
-#define OPEN_SIMPLEX_2_NOISE_HPP
+#ifndef SIMPLEX_NOISE_HPP
+#define SIMPLEX_NOISE_HPP
 
 #include <cstdint>
 #include <array>
-
-enum class OpenSimplex2Variant
-{
-	Fast,
-	Smooth
-};
 
 using ivec2 = std::array<int, 2>;
 using ivec3 = std::array<int, 3>;
 using dvec2 = std::array<double, 2>;
 using dvec3 = std::array<double, 3>;
 
-class OpenSimplex2Noise
+class SimplexNoise
 {
 private:
 	std::uint64_t seed_;
 
 public:
-	OpenSimplex2Noise(std::uint64_t seed);
+	SimplexNoise(std::uint64_t seed);
 
 	double Noise(double x, double y) const noexcept;
 
@@ -31,7 +25,8 @@ public:
 	std::uint64_t Seed() const noexcept { return seed_; }
 
 private:
+	void GetP2P3Offsets(const dvec3& p1, ivec3& p2_offsets, ivec3& p3_offsets) const noexcept;
 };
 
-#endif // OPEN_SIMPLEX_2_NOISE_HPP
+#endif // SIMPLEX_NOISE_HPP
 
