1437612d96fe2ece62c3dad6f599b7fa5d05ce0c
Mario <mariozak@tutanota.com>
2026-04-27 17:46:53 +0200

    OpenSimplex2 port of github java repo to c++

--- VoxTerGenAlgorithms/include/VoxTerGenAlgorithms/noise/core/OpenSimplex2F.hpp
+++ VoxTerGenAlgorithms/include/VoxTerGenAlgorithms/noise/core/OpenSimplex2F.hpp
@@ -1,14 +1,8 @@
-#ifndef OPEN_SIMPLEX_2_NOISE_HPP
-#define OPEN_SIMPLEX_2_NOISE_HPP
+#ifndef OPEN_SIMPLEX_2_F_HPP
+#define OPEN_SIMPLEX_2_F_HPP
 
 #include <cstdint>
 #include <array>
-
-enum class OpenSimplex2Variant
-{
-	Fast, 
-	Smooth
-};
 
 enum class Noise2DModifier
 {
@@ -42,41 +36,38 @@ using uivec3 = std::array<std::uint64_t, 3>;
 using dvec2 = std::array<double, 2>;
 using dvec3 = std::array<double, 3>;
 
-class OpenSimplex2Noise
+class OpenSimplex2F
 {
 private:
 	std::uint64_t seed_;
-	OpenSimplex2Variant variant_;
 	Noise2DModifier noise_2d_modifier_;
 	Noise3DModifier noise_3d_modifier_;
 	Noise4DModifier noise_4d_modifier_;
 
 public:
-	OpenSimplex2Noise(
+	OpenSimplex2F(
 		std::uint64_t seed, 
-		OpenSimplex2Variant variant, 
 		Noise2DModifier noise_2d_modifier = Noise2DModifier::Default,
-		Noise3DModifier noise_3d_modifier = Noise3DModifier::Default);
+		Noise3DModifier noise_3d_modifier = Noise3DModifier::Default, 
+		Noise4DModifier noise_4d_modifier = Noise4DModifier::Default);
 
 	double Noise(double x, double y) const noexcept;
 
 	double Noise(double x, double y, double z) const noexcept;
 
-	//double Noise(double x, double y, double z, double w) const noexcept;
+	double Noise(double x, double y, double z, double w) const noexcept;
 
 	// Getters
 	std::uint64_t Seed() const noexcept { return seed_; }
-	OpenSimplex2Variant Variant() const noexcept { return variant_; }
 	Noise2DModifier GetNoise2DModifier() const noexcept { return noise_2d_modifier_; }
 	Noise3DModifier GetNoise3DModifier() const noexcept { return noise_3d_modifier_; }
-	//Noise4DModifier GetNoise4DModifier() const noexcept { return noise_4d_modifier_; }
+	Noise4DModifier GetNoise4DModifier() const noexcept { return noise_4d_modifier_; }
 
 	// Setters
 	void SetSeed(std::uint64_t seed) noexcept { seed_ = seed; }
-	void SetVariant(OpenSimplex2Variant variant) noexcept { variant_ = variant; }
 	void SetNoise2DModifier(Noise2DModifier noise_2d_modifier) noexcept { noise_2d_modifier_ = noise_2d_modifier; }
 	void SetNoise3DModifier(Noise3DModifier noise_3d_modifier) noexcept { noise_3d_modifier_ = noise_3d_modifier; }
-	//void SetNoise4DModifier(Noise4DModifier noise_4d_modifier) noexcept { noise_4d_modifier_ = noise_4d_modifier; }
+	void SetNoise4DModifier(Noise4DModifier noise_4d_modifier) noexcept { noise_4d_modifier_ = noise_4d_modifier; }
 
 private:
 	void RotateCoords(double& xr, double& yr) const noexcept;
@@ -84,11 +75,6 @@ private:
 	void RotateCoords(double& xr, double& yr, double& zr) const noexcept;
 	
 	void RotateCoords(double& xr, double& yr, double& zr, double& wr) const noexcept;
-
-	double NoiseFast(double x, double y, double z) const noexcept;
-
-	double NoiseSmooth(double x, double y, double z) const noexcept;
 };
 
-#endif // OPEN_SIMPLEX_2_NOISE_HPP
-
+#endif // OPEN_SIMPLEX_2_F_HPP
---
+++ VoxTerGenAlgorithms/include/VoxTerGenAlgorithms/noise/core/OpenSimplex2S.hpp
@@ -0,0 +1,80 @@
+#ifndef OPEN_SIMPLEX_2_S_HPP
+#define OPEN_SIMPLEX_2_S_HPP
+
+#include <cstdint>
+#include <array>
+
+enum class Noise2DModifier
+{
+	Skew, 
+	Default = Skew,
+	ImproveX, 
+};
+
+enum class Noise3DModifier
+{
+	ImproveXY, 
+	ImproveXZ, 
+	Default = ImproveXZ,
+	Fallback
+};
+
+enum class Noise4DModifier
+{
+	ImproveXYZ, 
+	Default = ImproveXYZ,
+	ImproveXYZ_ImproveXY, 
+	ImproveXYZ_ImproveXZ, 
+	ImproveXY_ImproveZW, 
+	Fallback
+};
+
+using ivec2 = std::array<std::int64_t, 2>;
+using ivec3 = std::array<std::int64_t, 3>;
+using uivec2 = std::array<std::uint64_t, 2>;
+using uivec3 = std::array<std::uint64_t, 3>;
+using dvec2 = std::array<double, 2>;
+using dvec3 = std::array<double, 3>;
+
+class OpenSimplex2S
+{
+private:
+	std::uint64_t seed_;
+	Noise2DModifier noise_2d_modifier_;
+	Noise3DModifier noise_3d_modifier_;
+	Noise4DModifier noise_4d_modifier_;
+
+public:
+	OpenSimplex2S(
+		std::uint64_t seed, 
+		Noise2DModifier noise_2d_modifier = Noise2DModifier::Default,
+		Noise3DModifier noise_3d_modifier = Noise3DModifier::Default, 
+		Noise4DModifier noise_4d_modifier = Noise4DModifier::Default);
+
+	double Noise(double x, double y) const noexcept;
+
+	double Noise(double x, double y, double z) const noexcept;
+
+	double Noise(double x, double y, double z, double w) const noexcept;
+
+	// Getters
+	std::uint64_t Seed() const noexcept { return seed_; }
+	Noise2DModifier GetNoise2DModifier() const noexcept { return noise_2d_modifier_; }
+	Noise3DModifier GetNoise3DModifier() const noexcept { return noise_3d_modifier_; }
+	Noise4DModifier GetNoise4DModifier() const noexcept { return noise_4d_modifier_; }
+
+	// Setters
+	void SetSeed(std::uint64_t seed) noexcept { seed_ = seed; }
+	void SetNoise2DModifier(Noise2DModifier noise_2d_modifier) noexcept { noise_2d_modifier_ = noise_2d_modifier; }
+	void SetNoise3DModifier(Noise3DModifier noise_3d_modifier) noexcept { noise_3d_modifier_ = noise_3d_modifier; }
+	void SetNoise4DModifier(Noise4DModifier noise_4d_modifier) noexcept { noise_4d_modifier_ = noise_4d_modifier; }
+
+private:
+	void RotateCoords(double& xr, double& yr) const noexcept;
+
+	void RotateCoords(double& xr, double& yr, double& zr) const noexcept;
+	
+	void RotateCoords(double& xr, double& yr, double& zr, double& wr) const noexcept;
+};
+
+#endif // OPEN_SIMPLEX_2_S_HPP
