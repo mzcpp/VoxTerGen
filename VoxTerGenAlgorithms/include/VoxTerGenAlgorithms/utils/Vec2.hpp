7289d78b3fe744317752ac5c4b18d5983571d55c
Mario <mariozak@tutanota.com>
2026-04-30 19:08:57 +0200

    vec classes wip

--- VoxTerGenAlgorithms/include/VoxTerGenAlgorithms/utils/Vec2.hpp
+++ VoxTerGenAlgorithms/include/VoxTerGenAlgorithms/utils/Vec2.hpp
@@ -1,7 +1,14 @@
 #ifndef VEC2_HPP
 #define VEC2_HPP
 
-template<typename T>
+#include <concepts>
+#include <cmath>
+#include <utility>
+
+template<typename Type>
+concept VecType = std::arithmetic<T> || std::floating_point<T>;
+
+template<VecType T>
 class Vec2
 {
 private:
@@ -9,7 +16,44 @@ private:
 	T y_;
 
 public:
+	Vec2() : x_(static_cast<T>(0)), y_(static_cast<T>(0))
+	{
+	}
 
+	Vec2(T x, T y) : x_(x), y_(y)
+	{
+	}
+
+	Vec2(const Vec2& other) = default;
+	Vec2& operator=(const Vec2& other) = default;
+
+	Vec2(Vec2&& other) = default;
+	Vec2& operator=(Vec2&& other) = default;
+	
+	void Normalize() noexcept
+	{
+		const T length = GetLength();
+
+		if (length == static_cast<T>(0))
+		{
+			return;
+		}
+
+		x_ /= length;
+		y_ /= length;
+	}
+
+	void SetLength(T length) noexcept
+	{
+		Normalize();
+		x_ *= length;
+		y_ *= length;
+	}
+
+	auto Length() const
+	{
+		return std::sqrt((x_ * x_) + (y_ * y_));
+	}
 };
 
 #endif
--- VoxTerGenAlgorithms/include/VoxTerGenAlgorithms/utils/Vec3.hpp
+++ VoxTerGenAlgorithms/include/VoxTerGenAlgorithms/utils/Vec3.hpp
@@ -1,16 +1,174 @@
 #ifndef VEC3_HPP
 #define VEC3_HPP
 
-template<typename T>
-class Vec3
+#include <concepts>
+#include <cmath>
+#include <utility>
+
+namespace vtg
 {
-private:
-	T x_;
-	T y_;
-	T z_;
+	template<typename Type>
+	concept VecType = std::integral<Type> || std::floating_point<Type>;
 
-public:
+	template<VecType T>
+	class Vec3
+	{
+	public:
+		T x_;
+		T y_;
+		T z_;
 
-};
+		constexpr Vec3() noexcept : x_(static_cast<T>(0)), y_(static_cast<T>(0)), z_(static_cast<T>(0))
+		{
+		}
+
+		constexpr Vec3(T x, T y, T z) noexcept : x_(x), y_(y), z_(z)
+		{
+		}
+
+		constexpr Vec3(T v) noexcept : x_(v), y_(v), z_(v)
+		{
+		}
+
+		constexpr T& operator[](std::size_t i) noexcept
+		{
+			return (&x_)[i];
+		}
+
+		constexpr T& operator[](std::size_t i) const noexcept
+		{
+			return (&x_)[i];
+		}
+
+		constexpr Vec3 operator+() const noexcept
+		{
+			return *this;
+		}
+
+		constexpr Vec3 operator-() const noexcept
+		{
+			return { -x_, -y_, -z_ };
+		}
+
+		constexpr Vec3& operator+=(const Vec3& rhs) const noexcept
+		{
+			x_ += rhs.x_;
+			y_ += rhs.y_;
+			z_ += rhs.z_;
+
+			return *this;
+		}
+
+		constexpr Vec3& operator-=(const Vec3& rhs) const noexcept
+		{
+			x_ -= rhs.x_;
+			y_ -= rhs.y_;
+			z_ -= rhs.z_;
+			
+			return *this;
+		}
+
+		constexpr Vec3& operator*=(T s) const noexcept
+		{
+			x_ *= s;
+			y_ *= s;
+			z_ *= s;
+			
+			return *this;
+		}
+
+		constexpr Vec3& operator/=(T s) const noexcept
+		{
+			x_ /= s;
+			y_ /= s;
+			z_ /= s;
+			
+			return *this;
+		}
+		
+		constexpr Vec3 Normalize()
+		{
+			const T length = GetLength();
+
+			if (length != static_cast<T>(0))
+			{
+				*this /= length;
+			}
+
+			return *this;
+		}
+
+		constexpr Vec3 Normalized()
+		{
+			Vec3 cpy = *this;
+
+			return cpy.Normalize();
+		}
+
+		void SetLength(T length) noexcept
+		{
+			Normalize();
+			x_ *= length;
+			y_ *= length;
+			z_ *= length;
+		}
+
+		auto Length() const
+		{
+			return std::sqrt((x_ * x_) + (y_ * y_) + (z_ * z_));
+		}
+
+		auto LengthSquared() const
+		{
+			return (x_ * x_) + (y_ * y_) + (z_ * z_);
+		}
+	};
+
+	template<VecType T>
+	constexpr Vec3<T> operator+(Vec3<T> a, const Vec3<T>& b)
+	{
+		return a += b;
+	}
+
+	template<VecType T>
+	constexpr Vec3<T> operator-(Vec3<T> a, const Vec3<T>& v)
+	{
+		return a -= b;
+	}
+
+	template<VecType T>
+	constexpr Vec3<T> operator*(T s, Vec3<T> v)
+	{
+		return v *= s;
+	}
+
+	template<VecType T>
+	constexpr Vec3<T> operator*(Vec3<T> v, T s)
+	{
+		return v *= s;
+	}
+
+	template<VecType T>
+	constexpr Vec3<T> operator/(Vec3<T> v, T s)
+	{
+		return v /= s;
+	}
+
+	template<VecType T>
+	constexpr T Dot(const Vec3<T>& a, const Vec3<T>& b)
+	{
+		return a.x_ * b.x_ + a.y_ * b.y_ + a.z_ * b.z_;
+	}
+	
+	template<VecType T>
+	constexpr Vec3<T> Cross(const Vec3<T>& a, const Vec3<T>& b)
+	{
+		return { 
+			a.y_ * b.z_ - a.z_ * b.y_, 
+			a.z_ * b.x_ - a.x_ * b.z_, 
+			a.x_ * b.y_ - a.y_ * b.x_ 
+		};
+	}
+}
 
 #endif
