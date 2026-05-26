45375999ee83e66e2ae9f78b1328cfb0d83a0b77
Mario <mariozak@tutanota.com>
2026-05-26 17:13:48 +0200

    collision resolution wip & refactor

--- VoxTerGenEngine/include/physics/CollisionSystem.hpp
+++ VoxTerGenEngine/include/physics/CollisionSystem.hpp
@@ -3,11 +3,80 @@
 
 #include "world/Observer.hpp"
 #include "world/Block.hpp"
+//#include "AABB.hpp"
 
 #include "glm/vec3.hpp"
 
 #include <concepts>
 #include <vector>
+
+struct AABB
+{
+    glm::dvec3 min_;
+    glm::dvec3 max_;
+
+    AABB(double min_x, double min_y, double min_z, double max_x, double max_y, double max_z)
+    {
+        min_ = { min_x, min_y, min_z };
+        max_ = { max_x, max_y, max_z };
+    }
+
+    AABB Expand(glm::dvec3 value)
+    {
+        AABB result;
+
+        if (value.x > 0.0)
+        {
+            result.max_.x = max_.x + value.x;
+        }
+        else
+        {
+            result.min_.x = min_.x + value.x;
+        }
+
+        if (value.y > 0.0)
+        {
+            result.max_.y = max_.y + value.y;
+        }
+        else
+        {
+            result.min_.y = min_.y + value.y;
+        }
+
+        if (value.z > 0.0)
+        {
+            result.max_.z = max_.z + value.z;
+        }
+        else
+        {
+            result.min_.z = min_.z + value.z;
+        }
+
+        return result;
+    }
+
+    AABB Grow(glm::dvec3 value)
+    {
+        AABB result;
+
+        result.max_.x = max_.x + value.x;
+        result.max_.y = max_.y + value.y;
+        result.max_.z = max_.z + value.z;
+
+        result.min_.x = min_.x - value.x;      
+        result.min_.y = min_.y - value.y;
+        result.min_.z = min_.z - value.z;
+
+        return result;
+    }
+
+    void Move(glm::dvec3 vec)
+    {
+        min_.x += vec.x;
+        min_.y += vec.y;
+        min_.z += vec.z;
+    }
+};
 
 class Block;
 class Observer;
@@ -22,29 +91,28 @@ private:
 public:
     CollisionSystem();
 
-    bool AABBIntersectsX(glm::dvec3 min_a, glm::dvec3 max_a, glm::dvec3 min_b, glm::dvec3 max_b) const noexcept;
+    bool AABBIntersectsX(AABB first, AABB second) const noexcept;
 
-    bool AABBIntersectsY(glm::dvec3 min_a, glm::dvec3 max_a, glm::dvec3 min_b, glm::dvec3 max_b) const noexcept;
+    bool AABBIntersectsY(AABB first, AABB second) const noexcept;
 
-    bool AABBIntersectsZ(glm::dvec3 min_a, glm::dvec3 max_a, glm::dvec3 min_b, glm::dvec3 max_b) const noexcept;
+    bool AABBIntersectsZ(AABB first, AABB second) const noexcept;
 
-    bool AABBIntersects(glm::dvec3 min_a, glm::dvec3 max_a, glm::dvec3 min_b, glm::dvec3 max_b) const noexcept;
+    bool AABBIntersects(AABB first, AABB second) const noexcept;
 
-    float GetClipX(glm::dvec3 min_a, glm::dvec3 max_a, glm::dvec3 min_b, glm::dvec3 max_b, float delta_x) const noexcept;
+    float GetClipX(AABB first, AABB second, float delta_x) const noexcept;
 
-    float GetClipY(glm::dvec3 min_a, glm::dvec3 max_a, glm::dvec3 min_b, glm::dvec3 max_b, float delta_y) const noexcept;
+    float GetClipY(AABB first, AABB second, float delta_y) const noexcept;
 
-    float GetClipZ(glm::dvec3 min_a, glm::dvec3 max_a, glm::dvec3 min_b, glm::dvec3 max_b, float delta_z) const noexcept;
+    float GetClipZ(AABB first, AABB second, float delta_z) const noexcept;
 
     glm::vec3 GetClippedMovementVector(BlockQuery auto&& world_block_query, const Observer& observer, glm::vec3 movement_vector) const noexcept
     {
-        glm::vec3 clipped_movement_vector = movement_vector;
         const glm::dvec3 observer_min_coords = observer.Pos();
         const glm::dvec3 observer_max_coords = { observer_min_coords.x + observer.Width(), observer_min_coords.y + observer.Height(), observer_min_coords.z + observer.Depth() };
         const glm::ivec3 observer_block_coords = observer.BlockPos();
         const glm::ivec3 start_coords = { observer_block_coords.x - 1, observer_block_coords.y - 1, observer_block_coords.z - 1 };
 
-        std::vector<glm::dvec3> neighbor_block_coords_vec;
+        std::vector<glm::dvec3> neighbor_blocks_coords;
 
         for (int y = 0; y < static_cast<int>(observer.Height()) + 2; ++y)
         {
@@ -53,27 +121,49 @@ public:
                 for (int x = 0; x < static_cast<int>(observer.Width()) + 2; ++x)
                 {
                     const glm::ivec3 neighbor_block_coords = { start_coords.x + x, start_coords.y + y, start_coords.z + z };
-                    const Block neighbor_block = world_block_query(neighbor_block_coords);
 
-                    if (!neighbor_block.IsSolid())
+                    if (!world_block_query(neighbor_block_coords).IsSolid())
                     {
                         continue;
                     }
 
                     const glm::dvec3 neighbor_block_coords = { static_cast<double>(neighbor_block_coords.x), static_cast<double>(neighbor_block_coords.y), static_cast<double>(neighbor_block_coords.z) };
-                    neighbor_block_coords_vec.push_back(neighbor_block_coords);
+                    neighbor_blocks_coords.push_back(neighbor_block_coords);
                 }
             }
         }
 
-        for (glm::dvec3 neighbor_min_pos : neighbor_block_coords_vec)
+        glm::vec3 clipped_movement_vector = movement_vector;
+
+        for (glm::dvec3 neighbor_block_min_coord : neighbor_blocks_coords)
         {
-            // TODO
-            const glm::dvec3 neighbor_max_pos = { neighbor_min_pos.x + 1.0, neighbor_min_pos.y + 1.0, neighbor_min_pos.z + 1.0 };
-            clipped_movement_vector.x = GetClipX(observer_min_coords, observer_max_coords, neighbor_min_pos, neighbor_max_pos, movement_vector.x);
-            clipped_movement_vector.y = GetClipY(observer_min_coords, observer_max_coords, neighbor_min_pos, neighbor_max_pos, movement_vector.y);
-            clipped_movement_vector.z = GetClipZ(observer_min_coords, observer_max_coords, neighbor_min_pos, neighbor_max_pos, movement_vector.z);
+            const glm::dvec3 neighbor_block_max_coord = { neighbor_block_min_coord.x + 1.0, neighbor_block_min_coord.y + 1.0, neighbor_block_min_coord.z + 1.0 };
+            const float clipped_x = GetClipX(observer_min_coords, observer_max_coords, neighbor_block_min_coord, neighbor_block_max_coord, movement_vector.x);
+            clipped_movement_vector.x = clipped_x > 0.0f ? std::fmin(clipped_x, clipped_movement_vector.x) : std::fmax(clipped_x, clipped_movement_vector.x);
         }
+
+        observer_min_coords.x += clipped_movement_vector.x;
+        observer_max_coords.x += clipped_movement_vector.x;
+
+        for (glm::dvec3 neighbor_block_min_coord : neighbor_blocks_coords)
+        {
+            const glm::dvec3 neighbor_block_max_coord = { neighbor_block_min_coord.x + 1.0, neighbor_block_min_coord.y + 1.0, neighbor_block_min_coord.z + 1.0 };
+            const float clipped_y = GetClipY(observer_min_coords, observer_max_coords, neighbor_block_min_coord, neighbor_block_max_coord, movement_vector.y);
+            clipped_movement_vector.y = clipped_y > 0.0f ? std::fmin(clipped_y, clipped_movement_vector.y) : std::fmax(clipped_y, clipped_movement_vector.y);
+        }
+
+        observer_min_coords.y += clipped_movement_vector.y;
+        observer_max_coords.y += clipped_movement_vector.y;
+
+        for (glm::dvec3 neighbor_block_min_coord : neighbor_blocks_coords)
+        {
+            const glm::dvec3 neighbor_block_max_coord = { neighbor_block_min_coord.x + 1.0, neighbor_block_min_coord.y + 1.0, neighbor_block_min_coord.z + 1.0 };
+            const float clipped_z = GetClipZ(observer_min_coords, observer_max_coords, neighbor_block_min_coord, neighbor_block_max_coord, movement_vector.z);
+            clipped_movement_vector.z = clipped_z > 0.0f ? std::fmin(clipped_z, clipped_movement_vector.z) : std::fmax(clipped_z, clipped_movement_vector.z);
+        }
+
+        observer_min_coords.z += clipped_movement_vector.z;
+        observer_max_coords.z += clipped_movement_vector.z;
 
         return clipped_movement_vector;
     }
--- VoxTerGenEngine/src/physics/CollisionSystem.cpp
+++ VoxTerGenEngine/src/physics/CollisionSystem.cpp
@@ -1,4 +1,5 @@
-#include "physics/CollisionSystem.hpp"
+#include "physics/CollisionSystem.hpp" // <<< physics??
+//#include "AABB.hpp"
 
 /**
  * @note Portion of this source code was adapted from an article by Andre Blunt.
@@ -9,44 +10,44 @@ CollisionSystem::CollisionSystem()
 {
 }
 
-bool CollisionSystem::AABBIntersectsX(glm::dvec3 min_a, glm::dvec3 max_a, glm::dvec3 min_b, glm::dvec3 max_b) const noexcept
+bool CollisionSystem::AABBIntersectsX(AABB first, AABB second) const noexcept
 {
-    return min_a.x < max_b.x && max_a.x > min_b.x; 
+    return first.min_.x < second.max_.x && first.max_.x > second.min_.x; 
 }
 
-bool CollisionSystem::AABBIntersectsY(glm::dvec3 min_a, glm::dvec3 max_a, glm::dvec3 min_b, glm::dvec3 max_b) const noexcept
+bool CollisionSystem::AABBIntersectsY(AABB first, AABB second) const noexcept
 {
-    return min_a.y < max_b.y && max_a.y > min_b.y;
+    return first.min_.y < second.max_.y && first.max_.y > second.min_.y;
 }
 
-bool CollisionSystem::AABBIntersectsZ(glm::dvec3 min_a, glm::dvec3 max_a, glm::dvec3 min_b, glm::dvec3 max_b) const noexcept
+bool CollisionSystem::AABBIntersectsZ(AABB first, AABB second) const noexcept
 {
-    return min_a.z < max_b.z && max_a.z > min_b.z;
+    return first.min_.z < second.max_.z && first.max_.z > second.min_.z;
 }
 
-bool CollisionSystem::AABBIntersects(glm::dvec3 min_a, glm::dvec3 max_a, glm::dvec3 min_b, glm::dvec3 max_b) const noexcept
+bool CollisionSystem::AABBIntersects(AABB first, AABB second) const noexcept
 {
-    return AABBIntersectsX(min_a, max_a, min_b, max_b) && AABBIntersectsY(min_a, max_a, min_b, max_b) && AABBIntersectsZ(min_a, max_a, min_b, max_b);
+    return AABBIntersectsX(first.min_, first.max_, second.min_, second.max_) && AABBIntersectsY(first.min_, first.max_, second.min_, second.max_) && AABBIntersectsZ(first.min_, first.max_, second.min_, second.max_);
 }
 
-float CollisionSystem::GetClipX(glm::dvec3 min_a, glm::dvec3 max_a, glm::dvec3 min_b, glm::dvec3 max_b, float delta_x) const noexcept
+float CollisionSystem::GetClipX(AABB first, AABB second, float delta_x) const noexcept
 {
-    if (!AABBIntersectsY(min_a, max_a, min_b, max_b) || !AABBIntersectsZ(min_a, max_a, min_b, max_b))
+    if (!AABBIntersectsY(first.min_, first.max_, second.min_, second.max_) || !AABBIntersectsZ(first.min_, first.max_, second.min_, second.max_))
     {
         return delta_x;
     }
 
-    if (delta_x > 0 && max_a[0] <= min_b[0])
+    if (delta_x > 0 && first.max_[0] <= second.min_[0])
     {
-        if (const float clip = min_b[0] - max_a[0] < delta_x)
+        if (const float clip = second.min_[0] - first.max_[0] < delta_x)
         {
             delta_x = clip;
         }
     }
 
-    if (delta_x < 0 && min_a[0] >= max_b[0])
+    if (delta_x < 0 && first.min_[0] >= second.max_[0])
     {
-        if (const float clip = max_b[0] - min_a[0] > delta_x)
+        if (const float clip = second.max_[0] - first.min_[0] > delta_x)
         {
             delta_x = clip;
         }
@@ -55,24 +56,24 @@ float CollisionSystem::GetClipX(glm::dvec3 min_a, glm::dvec3 max_a, glm::dvec3 m
     return delta_x;
 }
 
-float CollisionSystem::GetClipY(glm::dvec3 min_a, glm::dvec3 max_a, glm::dvec3 min_b, glm::dvec3 max_b, float delta_y) const noexcept
+float CollisionSystem::GetClipY(AABB first, AABB second, float delta_y) const noexcept
 {
-    if (!AABBIntersectsX(min_a, max_a, min_b, max_b) || !AABBIntersectsZ(min_a, max_a, min_b, max_b))
+    if (!AABBIntersectsX(first.min_, first.max_, second.min_, second.max_) || !AABBIntersectsZ(first.min_, first.max_, second.min_, second.max_))
     {
         return delta_y;
     }
 
-    if (delta_y > 0 && max_a[1] <= min_b[1])
+    if (delta_y > 0 && first.max_[1] <= second.min_[1])
     {
-        if (const float clip = min_b[1] - max_a[1] < delta_y)
+        if (const float clip = second.min_[1] - first.max_[1] < delta_y)
         {
             delta_y = clip;
         }
     }
 
-    if (delta_y < 0 && min_a[1] >= max_b[1])
+    if (delta_y < 0 && first.min_[1] >= second.max_[1])
     {
-        if (const float clip = max_b[1] - min_a[1] > delta_y)
+        if (const float clip = second.max_[1] - first.min_[1] > delta_y)
         {
             delta_y = clip;
         }
@@ -81,24 +82,24 @@ float CollisionSystem::GetClipY(glm::dvec3 min_a, glm::dvec3 max_a, glm::dvec3 m
     return delta_y;
 }
 
-float CollisionSystem::GetClipZ(glm::dvec3 min_a, glm::dvec3 max_a, glm::dvec3 min_b, glm::dvec3 max_b, float delta_z) const noexcept
+float CollisionSystem::GetClipZ(AABB first, AABB second, float delta_z) const noexcept
 {
-    if (!AABBIntersectsX(min_a, max_a, min_b, max_b) || !AABBIntersectsY(min_a, max_a, min_b, max_b))
+    if (!AABBIntersectsX(first.min_, first.max_, second.min_, second.max_) || !AABBIntersectsY(first.min_, first.max_, second.min_, second.max_))
     {
         return delta_z;
     }
 
-    if (delta_z > 0 && max_a[2] <= min_b[2])
+    if (delta_z > 0 && first.max_[2] <= second.min_[2])
     {
-        if (const float clip = min_b[2] - max_a[2] < delta_z)
+        if (const float clip = second.min_[2] - first.max_[2] < delta_z)
         {
             delta_z = clip;
         }
     }
 
-    if (delta_z < 0 && min_a[2] >= max_b[2])
+    if (delta_z < 0 && first.min_[2] >= second.max_[2])
     {
-        if (const float clip = max_b[2] - min_a[2] > delta_z)
+        if (const float clip = second.max_[2] - first.min_[2] > delta_z)
         {
             delta_z = clip;
         }
