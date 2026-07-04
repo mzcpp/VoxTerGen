936d0ae6df3853fa86357bd2a8621d6511c0c856
Mario <mariozak@tutanota.com>
2026-07-04 22:20:48 +0200

    dda setup

--- VoxTerGenEngine/include/physics/DigitalDifferentialAnalyzer.hpp
+++ VoxTerGenEngine/include/physics/DigitalDifferentialAnalyzer.hpp
@@ -1,12 +1,29 @@
 #ifndef DIGITAL_DIFFERENTIAL_ANALYZER_HPP
 #define DIGITAL_DIFFERENTIAL_ANALYZER_HPP
 
+#include "core/Direction.hpp"
+
+#include "world/Block.hpp"
+
+#include <glm/vec3.hpp>
+
+struct RaycastResult
+{
+	glm::ivec3 block_coords_;
+	BlockType type_;
+	Direction face_;
+	glm::dvec3 intersection_;
+};
+
+template <typename Fnc>
+concept BlockQuery = std::invocable<Fnc, glm::ivec3>&& std::convertible_to<std::invoke_result_t<Fnc, glm::ivec3>, BlockInfo>;
+
 class DigitalDifferentialAnalyzer
 {
 private:
 
 public:
-
+	static RaycastResult CastRay(glm::dvec3 start_pos, glm::dvec3 dir, BlockQuery auto&& world_block_query);
 };
 
 #endif // DIGITAL_DIFFERENTIAL_ANALYZER_HPP
--- VoxTerGenEngine/src/physics/DigitalDifferentialAnalyzer.cpp
+++ VoxTerGenEngine/src/physics/DigitalDifferentialAnalyzer.cpp
@@ -1,1 +1,6 @@
 #include "physics/DigitalDifferentialAnalyzer.hpp"
+
+RaycastResult DigitalDifferentialAnalyzer::CastRay(glm::dvec3 start_pos, glm::dvec3 dir, BlockQuery auto&& world_block_query)
+{
+	return {};
+}
