453617b53e37c5f3204bf4c11f0f9e10c29148db
Mario <mariozak@tutanota.com>
2026-09-08 16:50:24 +0200

    FBM wip

--- VoxTerGenAlgorithms/include/VoxTerGenAlgorithms/noise/methods/FractionalBrownianMotion.hpp
+++ VoxTerGenAlgorithms/include/VoxTerGenAlgorithms/noise/methods/FractionalBrownianMotion.hpp
@@ -1,12 +1,19 @@
 #ifndef FRACTIONAL_BROWNIAN_MOTION_HPP
 #define FRACTIONAL_BROWNIAN_MOTION_HPP
 
+#include "VoxTerGenAlgorithms/utils/Common.hpp"
+
+template <NoiseType Noise>
 class FractionalBrownianMotion
 {
 private:
-    
-public:
+    Noise noise_;
 
+public:
+    FractionalBrownianMotion()
+    {
+        
+    }
 };
 
 #endif // FRACTIONAL_BROWNIAN_MOTION_HPP
