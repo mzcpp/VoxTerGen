a172267cffc43c93b35ccb3faeba0782024b4e27
Mario <mariozak@tutanota.com>
2026-09-25 20:36:04 +0200

    removed useless file and polished vector field walker

--- VoxTerGenAlgorithms/include/VoxTerGenAlgorithms/noise/methods/VectorFieldWalker.hpp
+++ VoxTerGenAlgorithms/include/VoxTerGenAlgorithms/noise/methods/VectorFieldWalker.hpp
@@ -8,33 +8,38 @@
 #include <array>
 #include <cmath>
 #include <concepts>
+#include <cstddef>
 #include <cstdint>
+#include <optional>
 #include <random>
 
-template<typename Field>
-concept Field2D = requires(const Field& f, double x, double y)
+template<typename ScalarField>
+concept Field2D = requires(const ScalarField & f, double x, double y)
 {
     { f.Sample(x, y) } -> std::convertible_to<double>;
 };
 
-
-template<typename Field>
-concept Field3D = requires(const Field& f, double x, double y, double z)
+template<typename ScalarField>
+concept Field3D = requires(const ScalarField & f, double x, double y, double z)
 {
     { f.Sample(x, y, z) } -> std::convertible_to<double>;
 };
 
-template <typename Field>
+template <typename ScalarField>
 class VectorFieldWalker
 {
 private:
-    Field field_;
-    std::array<double, 6> offsets_;
+    ScalarField scalar_field_;
+    std::array<double, 9> offsets_;
     double step_size_;
+    double frequency_;
 
 public:
-    VectorFieldWalker(const Field& field, std::uint64_t seed, double step_size) : field_(field), step_size_(step_size), offsets_(0.0)
+    VectorFieldWalker(const ScalarField& field, std::uint64_t seed, double step_size, double frequency) : scalar_field_(field), offsets_(0.0)
     {
+        step_size_ = std::isfinite(step_size) && step_size > 0.0 ? step_size : 1.0;
+        frequency_ = std::isfinite(frequency) && frequency > 0.0 ? frequency : 0.0025;
+
         constexpr double low_range = -1000.0;
         constexpr double high_range = 1000.0;
 
@@ -47,41 +52,62 @@ public:
         }
     }
 
-    dvec2 Step(const dvec2& start_pos) const requires Field2D<Field>
+    std::optional<dvec2> Step(const dvec2& start_pos) const requires Field2D<ScalarField>
     {
-        const double new_pos_dir_x = field_.Sample(start_pos[0], start_pos[1]);
-        const double new_pos_dir_y = field_.Sample(start_pos[0] + offsets_[0], start_pos[1] + offsets_[1]);
+        const double new_pos_dir_x = scalar_field_.Sample(
+            (start_pos[0] + offsets_[0]) * frequency_, 
+            (start_pos[1] + offsets_[1]) * frequency_
+        );
+
+        const double new_pos_dir_y = scalar_field_.Sample(
+            (start_pos[0] + offsets_[2]) * frequency_, 
+            (start_pos[1] + offsets_[3]) * frequency_
+        );
 
         const math::Vec2 start = { start_pos[0], start_pos[1] };
         const math::Vec2 new_pos_dir = { new_pos_dir_x, new_pos_dir_y };
         
         if (!std::isfinite(new_pos_dir_x) || !std::isfinite(new_pos_dir_y) || new_pos_dir.LengthSquared() < 1e-6)
         {
-            return start_pos;
+            return std::nullopt;
         }
 
         const math::Vec2 new_pos = start + new_pos_dir.Normalized() * step_size_;
 
-        return { new_pos.x_, new_pos.y_ };
+        return std::optional<dvec2>{ new_pos.x_, new_pos.y_ };
     }
 
-    dvec3 Step(const dvec3& start_pos) const requires Field3D<Field>
+    std::optional<dvec3> Step(const dvec3& start_pos) const requires Field3D<ScalarField>
     {
-        const double new_pos_dir_x = field_.Sample(start_pos[0], start_pos[1], start_pos[2]);
-        const double new_pos_dir_y = field_.Sample(start_pos[0] + offsets_[0], start_pos[1] + offsets_[1], start_pos[2] + offsets_[2]);
-        const double new_pos_dir_z = field_.Sample(start_pos[0] + offsets_[3], start_pos[1] + offsets_[4], start_pos[2] + offsets_[5]);
+        const double new_pos_dir_x = scalar_field_.Sample(
+            (start_pos[0] + offsets_[0]) * frequency_, 
+            (start_pos[1] + offsets_[1]) * frequency_, 
+            (start_pos[2] + offsets_[2]) * frequency_
+        );
+        
+        const double new_pos_dir_y = scalar_field_.Sample(
+            (start_pos[0] + offsets_[3]) * frequency_, 
+            (start_pos[1] + offsets_[4]) * frequency_, 
+            (start_pos[2] + offsets_[5]) * frequency_
+        );
+        
+        const double new_pos_dir_z = scalar_field_.Sample(
+            (start_pos[0] + offsets_[6]) * frequency_, 
+            (start_pos[1] + offsets_[7]) * frequency_, 
+            (start_pos[2] + offsets_[8]) * frequency_
+        );
 
         const math::Vec3 start = { start_pos[0], start_pos[1], start_pos[2] };
         const math::Vec3 new_pos_dir = { new_pos_dir_x, new_pos_dir_y, new_pos_dir_z };
 
         if (!std::isfinite(new_pos_dir_x) || !std::isfinite(new_pos_dir_y) || !std::isfinite(new_pos_dir_z) || new_pos_dir.LengthSquared() < 1e-6)
         {
-            return start_pos;
+            return std::nullopt;
         }
 
         const math::Vec3 new_pos = start + new_pos_dir.Normalized() * step_size_;
         
-        return { new_pos.x_, new_pos.y_, new_pos.z_ };
+        return std::optional<dvec3>{ new_pos.x_, new_pos.y_, new_pos.z_ };
     }
 };
 
